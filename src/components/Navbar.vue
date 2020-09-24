152e0bce4689c54c81257b3e9d55d16aa690db79
Pynshik <anst.tkach@gmail.com>
2020-09-24 23:07:41 +0300

    tasks list

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav class="teal darken-1">
+      <div class="nav-wrapper teal darken-1">
+        <router-link to="/" class="brand-logo">Tasks</router-link>
+          <ul class="right hide-on-med-and-down">
+            <router-link
+              tag="li"
+              to="/"
+              exact
+              active-class="active"
+            >
+              <a href="#">Create</a>
+              </router-link>
+              <router-link
+              tag="li"
+              to="/list"
+              active-class="active"
+            >
+              <a href="#">List</a>
+              </router-link>
+          </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+  nav {
+    padding: 0 0 0 2rem;
+  }
+</style>
